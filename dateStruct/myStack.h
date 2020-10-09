7bc3f89c7023144835bf6789b1d237f2734f3a17
hangcaihui <wangzhe177@bupt.edu.cn>
2020-10-09 22:18:29 +0800

    项目配置

--- dateStruct/myStack.h
+++ dateStruct/myStack.h
@@ -1,26 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef BASE
 #define NUMofFUN 6
-#define LENOFSTACK  sizeof(struct data) * 1000
-char opr[5] = { '+','-','*','/' ,'^'};
-char bra[6] = { '[',']','{','}','(',')' };
-char func[26][6] = { "sin","cos","ln","tan","log","!"};
-enum flager {
-	Num= 0,Fun,Ope,Bra,Not
+#define LENOFSTACK sizeof(struct data) * 1000
+extern char opr[5];
+extern char bra[6];
+extern char func[26][6];
+enum flager
+{
+	Num = 0,
+	Fun,
+	Ope,
+	Bra,
+	Not
 };
-struct data {
-	enum flager flag;//1--ch, 0--num,
+struct data
+{
+	enum flager flag; //1--ch, 0--num,
 	char ch;
 	double num;
 };
 
 struct STACKER
 {
-    int i;         //用于判断是字符还是数字还是空栈
-    int flagOfTop; //用于计算栈的个数
-    struct data dataInStack[LENOFSTACK];
+	int i;		   //用于判断是字符还是数字还是空栈
+	int flagOfTop; //用于计算栈的个数
+	struct data dataInStack[LENOFSTACK];
 };
-
+#endif
 
 typedef struct STACKER *STACK;
+typedef struct data nodeOfStack;
 int isEmpty(STACK);
 void makeEmpty(STACK);
 void push(STACK, struct data);
--- dateStruct/reverse.c
+++ dateStruct/reverse.c
@@ -1,49 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define NUMofFUN 6
-#define LENOFSTACK sizeof(struct data) * 1000
+#include "myStack.h"
+
+int isLower(nodeOfStack A, nodeOfStack B);
+void printNode(nodeOfStack temp);
 char opr[5] = {'+', '-', '*', '/', '^'};
 char bra[6] = {'[', ']', '{', '}', '(', ')'};
 char func[26][6] = {"sin", "cos", "ln", "tan", "log", "!"};
-enum flager
-{
-    Num = 0,
-    Fun,
-    Ope,
-    Bra,
-    Not
-};
-struct data
-{
-    enum flager flag; //1--ch, 0--num,
-    char ch;
-    double num;
-};
-
-struct STACKER
-{
-    int i;         //用于判断是字符还是数字还是空栈
-    int flagOfTop; //用于计算栈的个数
-    struct data dataInStack[LENOFSTACK];
-};
-
-typedef struct STACKER *STACK;
-typedef struct data nodeOfStack;
-int isEmpty(STACK);
-void makeEmpty(STACK);
-void push(STACK, struct data);
-struct data top(STACK);
-void pop(STACK);
-struct data topAndPop(STACK);
-STACK createStack(STACK);
-struct data makeNode(double a, char b); //若为数字，则b默认为@
-void printStack(STACK);
-void printNode(nodeOfStack);
-int isLower(nodeOfStack A, nodeOfStack B);//A小于等于B返回1
 int main()
 {
     nodeOfStack A;
-    STACK BA=createStack(BA);
+    STACK BA = createStack(BA);
     nodeOfStack temp;
     A = makeNode(12.0, '@');
     switch (A.flag)
@@ -52,16 +17,17 @@ int main()
         printf("%lf", A.num);
         break;
     case Bra:
-        if (A.ch=='(')
+        if (A.ch == '(')
         {
             push(BA, A);
-        }else
+        }
+        else
         {
             temp = topAndPop(BA);
-            while (temp.ch!=')')
+            while (temp.ch != ')')
             {
                 temp = top(BA);
-                if (temp.flag!=Bra)
+                if (temp.flag != Bra)
                 {
                     printNode(temp);
                 }
@@ -71,16 +37,17 @@ int main()
                 }
                 pop(BA);
             }
-            
         }
-    case Ope:case Fun://A是新建的节点
-        if (isLower(top(BA),A)||top(BA).flag==Bra)
+    case Ope:
+    case Fun: //A是新建的节点
+        if (isLower(top(BA), A) || top(BA).flag == Bra)
         {
             push(BA, A);
-        }else
+        }
+        else
         {
             temp = topAndPop(BA);
-            while (! isLower(temp,A))
+            while (!isLower(temp, A))
             {
                 printNode(temp);
                 temp = topAndPop(BA);
@@ -88,21 +55,19 @@ int main()
             push(BA, A);
             printNode(temp);
         }
-        
-        
-        
-        
+
     default:
         while (!isEmpty(BA))
         {
             temp = topAndPop(BA);
             printNode(temp);
         }
-        
+
         break;
     }
-    
-    
+
+    printf("编译成功");
+    getchar();
 }
 void printNode(nodeOfStack temp)
 {
