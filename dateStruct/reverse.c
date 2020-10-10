14deee8e6da32821b8a80e6dd1b4607f865ff461
hangcaihui <wangzhe177@bupt.edu.cn>
2020-10-10 20:54:36 +0800

    超级模块化

--- dateStruct/reverse.c
+++ dateStruct/reverse.c
@@ -6,6 +6,8 @@ double mathOfFun(double, char);
 void specialPush(STACK, nodeOfStack);
 double reverse(nodeOfStack[]);
 void printNode(nodeOfStack temp);
+void dealWithBra(nodeOfStack, STACK, STACK,int*);
+void dealWithFun(nodeOfStack, STACK, STACK);
 char opr[5] = {'+', '-', '*', '/', '^'};
 char bra[6] = {'[', ']', '{', '}', '(', ')'};
 char func[26][6] = {"sin", "cos", "ln", "tan", "log", "!"};
@@ -43,6 +45,60 @@ int isLower(nodeOfStack A, nodeOfStack B)
 {
     return A.ch <= B.ch;
 }
+void dealWithBra(nodeOfStack A, STACK BA, STACK AB,int* i)
+{
+    if (A.ch == '(')
+    {
+        (*i)++;
+        push(BA, A);
+    }
+    else
+    {
+        (*i)--;
+        nodeOfStack temp = topAndPop(BA);
+        while (temp.ch != ')')
+        {
+            temp = top(BA);
+            if (temp.flag != Bra)
+            {
+                specialPush(AB, temp);
+                //printNode(temp);
+            }
+            else
+            {
+                pop(BA);
+            }
+            pop(BA);
+        }
+    }
+}
+void dealWithFun(nodeOfStack A, STACK BA, STACK AB)
+{
+    if (isLower(top(BA), A) || top(BA).flag == Bra || isEmpty(BA))
+    {
+        push(BA, A);
+    }
+    else
+    {
+        nodeOfStack temp = topAndPop(BA);
+        while (!isLower(temp, A))
+        {
+            //通过函数实现
+            specialPush(AB, temp);
+            temp = topAndPop(BA);
+        }
+        push(BA, A);
+        //printNode(temp);
+    }
+}
+void dealWithDefault(STACK AB,STACK BA)
+{
+    while (!isEmpty(BA))
+    {
+        nodeOfStack temp = topAndPop(BA);
+        specialPush(AB, temp);
+    }
+}
 double reverse(nodeOfStack first[])
 {
     int numOfNode = sizeof(first) / sizeof(nodeOfStack);
@@ -54,65 +110,22 @@ double reverse(nodeOfStack first[])
     while (i)
     {
         nodeOfStack A = first[i];
-        nodeOfStack temp;
         switch (A.flag)
         {
         case Num:
-            push(AB, A);
-            //printf("%lf", A.num);
+            push(AB, A);//printf("%lf", A.num);
             break;
         case Bra:
-            if (A.ch == '(')
-            {
-                i++;
-                push(BA, A);
-            }
-            else
-            {
-                i--;
-                temp = topAndPop(BA);
-                while (temp.ch != ')')
-                {
-                    temp = top(BA);
-                    if (temp.flag != Bra)
-                    {
-                        specialPush(AB, temp);
-                        //printNode(temp);
-                    }
-                    else
-                    {
-                        pop(BA);
-                    }
-                    pop(BA);
-                }
-            }
+            dealWithBra(A, BA, AB, &i);
+            break;
         case Ope:
         case Fun: //A是新建的节点
-            if (isLower(top(BA), A) || top(BA).flag == Bra || isEmpty(BA))
-            {
-                push(BA, A);
-            }
-            else
-            {
-                temp = topAndPop(BA);
-                while (!isLower(temp, A))
-                {
-                    //通过函数实现
-                    specialPush(AB, temp);
-                    temp = topAndPop(BA);
-                }
-                push(BA, A);
-                //printNode(temp);
-            }
+            dealWithFun(A, BA, AB);
+            break;
         default: //读入等号
-            while (!isEmpty(BA))
-            {
-                temp = topAndPop(BA);
-                specialPush(AB, temp);
-            }
+            dealWithDefault(AB, BA);
             break;
         }
-
         printf("编译成功");
         getchar();
     }
